2690d743a5583af10a4267a987bd4f67b304f896
rajahn01 <rafayuno@gmail.com>
2023-06-08 01:33:12 -0300

    feat(healthbar): healthbar interaction and ui

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Sprite {
 
         this.color = color;
         this.isAttacking
+        this.health = 100;
     }
 
     draw() {
@@ -171,8 +172,9 @@ function animate(){
         }) &&
         player.isAttacking
         ) {
-        console.log('hit');
         player.isAttacking = false;
+        enemy.health -= 10
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%';
     }
 
     if (
